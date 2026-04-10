0281c04b428d4befd9b06a05c261736c87007acc
Ramashish <ramashish.gupta@observe.ai>
2026-04-10 15:39:00 +0530

    version upgrade to 0.1.4

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ DESCRIPTION = "The official Python SDK for SpeechCortex ASR platform."
 
 setup(
     name="speechcortex-sdk",
-    version="0.1.3",
+    version="0.1.4",
     author="SpeechCortex Team",
     author_email="team@speechcortex.com",
     url="https://github.com/speechcortex/speechcortex-sdk",
